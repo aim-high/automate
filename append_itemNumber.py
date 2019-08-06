5ee35c77872ee4fc92eb31db73bf0b91b52e2aa9
Erica Ching <36242984+eching11@users.noreply.github.com>
2019-08-06 15:57:56 -0700

    Update append_itemNumber
    Added description.

---
+++ append_itemNumber.py
@@ -0,0 +1,21 @@
+# Description: Input tech name (i.e. "iPad_"), amount of that category, and item number (i.e. 23, 09, 38). Output is concatenated tech name and number.
+# Purpose: Easily manage large inventory of tech equipment when loaning and receiving from sites. 
+# Author: Erica Ching (eching@aimhigh.org)
+
+#!/usr/bin/python
+import sys
+
+name = input("Name: ")
+num = int(input("How many items?: "))
+list = [];
+numLoop = num;
+
+while num != 0:
+	item = input("Item number: ")
+	list.append(name + item)
+	num = num - 1
+
+i = 0;
+for numLoop in list:
+	print(list[i])
+	i = i + 1
